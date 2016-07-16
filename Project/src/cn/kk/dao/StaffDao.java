d312be2e8e7c709ad49337a146f6508d506558f5
dlut_maruixin_guodeji <812966201@qq.com>
2016-07-16 23:31:04 +0800

    last commit today

--- Project/src/cn/kk/dao/StaffDao.java
+++ Project/src/cn/kk/dao/StaffDao.java
@@ -1,5 +1,9 @@
 package cn.kk.dao;
 
-public interface StaffDao {
+import cn.kk.entity.Asset;
+import cn.kk.entity.Staff;
 
+public interface StaffDao {
+	int lend(Staff s,Asset a);
+	int returnAsset(Asset a);
 }
