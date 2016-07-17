54d254222e3c840fff99adcb04aa89431c25f38e
dlut_maruixin_guodeji <812966201@qq.com>
2016-07-17 21:13:20 +0800

    管理层全部实现

--- Project/src/cn/kk/manage/AdministratorManage.java
+++ Project/src/cn/kk/manage/AdministratorManage.java
@@ -1,5 +1,36 @@
 package cn.kk.manage;
 
-public class AdministratorManage {
+import cn.kk.dao.AdministratorDao;
+import cn.kk.dao.AdministratorImpl;
+import cn.kk.entity.Administrator;
+/**
+ * 
+ * @author Guo Deji
+ * @create 2016-07-17
+ */
 
+public class AdministratorManage {
+	/**
+	 * Administrator DAO implement
+	 */
+	AdministratorDao admindao=new AdministratorImpl();
+	
+	/**
+	 * login
+	 * @param name
+	 * @param pwd
+	 * @return
+	 */
+	public Administrator login(String name,String pwd){
+		return admindao.find(name, pwd);
+	}
+	/**
+	 * set password
+	 * @param ad
+	 * @param pwd
+	 * @return
+	 */
+	public int setPwd(Administrator ad,String pwd){
+		return admindao.setPwd(ad, pwd);
+	}
 }
--- Project/src/cn/kk/manage/AssetManage.java
+++ Project/src/cn/kk/manage/AssetManage.java
@@ -1,5 +1,95 @@
 package cn.kk.manage;
 
-public class AssetManage {
+import java.util.List;
 
+import cn.kk.dao.AssetDao;
+import cn.kk.dao.AssetImpl;
+import cn.kk.entity.Asset;
+
+public class AssetManage {
+	/**
+	 * Asset DAO implement
+	 */
+	AssetDao assetdao=new AssetImpl();
+	/**
+	 * add asset
+	 * @param a
+	 * @return
+	 */
+	public int add(Asset a){
+		return assetdao.add(a);
+	}
+	/**
+	 * update asset
+	 * @param a
+	 * @return
+	 */
+	public int update(Asset a){
+		return assetdao.update(a);
+	}
+	/**
+	 * delete asset
+	 * @param a
+	 * @return
+	 */
+	public int delete(Asset a){
+		return assetdao.delete(a);
+	}
+	/**
+	 * add category or subCategory
+	 * @param subCtg
+	 * @param ctg
+	 * @return
+	 */
+	public int addCategory(String subCtg,String ctg){
+		return assetdao.addCategory(subCtg, ctg);
+	}
+	/**
+	 * delete subCatrgory
+	 * @param subCtg
+	 * @return
+	 */
+	public int deleteSubCategory(String subCtg){
+		return assetdao.deleteSubCategory(subCtg);
+	}
+	/**
+	 * delete category
+	 * @param ctg
+	 * @return
+	 */
+	public int deleteCategory(String ctg){
+		return assetdao.deleteCategory(ctg);
+	}
+	/**
+	 * query by Category
+	 * @param ctg
+	 * @return
+	 */
+	public List<Asset> queryByCtg(String ctg){
+		return assetdao.queryByCtg(ctg);
+	}
+	/**
+	 * query by subCategory
+	 * @param subCtg
+	 * @return
+	 */
+	public List<Asset> queryBySubCtg(String subCtg){
+		return assetdao.queryBySubCtg(subCtg);
+	}
+	/**
+	 * query by ID
+	 * @param id
+	 * @return
+	 */
+	public Asset queryById(int id){
+		return assetdao.queryById(id);
+	}
+	/**
+	 * query by user
+	 * @param user
+	 * @return
+	 */
+	public List<Asset> queryByUser(String user){
+		return assetdao.queryByUser(user);
+	}
 }
