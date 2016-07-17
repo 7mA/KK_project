a2cf45ba9129beeade8d5e15a0c03f553a28e7c1
dlut_maruixin_guodeji <812966201@qq.com>
2016-07-17 10:11:07 +0800

    AdministratorImpl.java

--- Project/src/cn/kk/dao/AssetImpl.java
+++ Project/src/cn/kk/dao/AssetImpl.java
@@ -1,5 +1,69 @@
 package cn.kk.dao;
 
-public class AssetImpl {
+import java.util.List;
 
+import cn.kk.entity.Asset;
+
+public class AssetImpl extends BaseDao implements AssetDao{
+
+	@Override
+	public int add(Asset a) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int update(Asset a) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int delete(Asset a) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int addCategory(String subCtg, String ctg) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int deleteSubCategory(String subCtg) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int deleteCategory(String ctg) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public List<Asset> queryByCtg(String ctg) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Asset> queryBySubCtg(String subCtg) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Asset> queryById(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Asset> queryByUser(String user) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
 }
